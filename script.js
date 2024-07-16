780780c4a8e922c736925945110b98b955e2bc7c
Shakir Ullah <shakirullahaup@gmail.com>
2024-07-17 01:41:45 +0500

    chap_10 done

---
+++ script.js
@@ -0,0 +1,54 @@
+// chap no 10 (if statment)
+
+// example
+const badal=false;
+if (badal){
+    alert("umberal utao");
+}
+else{
+    alert("umberal na utao");
+}
+
+const paper=false;
+if(paper){
+    alert("jao paper ka lai");
+}
+else{
+    alert("na jao paper kai lia");
+}
+
+let userAge=+prompt("please enter your age")
+
+if (userAge >=18){
+    alert("you are eligibal");
+}
+else{
+    alert("youb are not eligibal");
+// }
+let userAge=("enter your age");
+if (userAge >=18){
+    alertt("you are eligibal");
+}
+if (userAge >18){
+    alert("you are not eligibal");
+}
+const cardBalance=+prompt("please enter your balance");
+if (cardBalance >=55){
+    alert("well come to peshawar zoo");
+}
+if (cardBalance <55){
+    alert("please recharge");
+}
+
+// const userName=("please enter your name")
+const userName=+prompt("please enter your name ")
+
+if (userName === 0){
+    alert(userName+"ap nai 0 enter kia");
+}
+if (userName < 0){
+    alert(userName+"ap ni negitve number enter kia");
+}
+if (userName >1){
+    alert(userName+"ap nai positive nomber enter kia");
+}
